288152e9c63dce613edf3a9403902749011290f4
Didem Temel <44496208+didemtemel@users.noreply.github.com>
2022-04-15 22:49:04 +0300

    Implement health check

--- src/test/java/restful/booker/Base.java
+++ src/test/java/restful/booker/Base.java
@@ -2,7 +2,11 @@ package restful.booker;
 
 import com.github.javafaker.Faker;
 import com.google.gson.Gson;
+import org.testng.annotations.BeforeClass;
 import restful.booker.service.BookingService;
+
+import static io.restassured.RestAssured.given;
+import static restful.booker.helper.Config.BOOKING_SERVICE_URL;
 
 public class Base {
     public static final Gson gson = new Gson();
@@ -10,5 +14,16 @@ public class Base {
     protected BookingService bookingService = new BookingService();
     protected Faker faker = new Faker();
 
+    @BeforeClass
+    public static void setUpClass() {
+        shouldBeHealthy();
+    }
+
+    private static void shouldBeHealthy() {
+        given()
+                .get(BOOKING_SERVICE_URL + "/ping").then()
+                .assertThat()
+                .statusCode(201);
+    }
 
 }
